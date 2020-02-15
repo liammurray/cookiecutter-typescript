f060b1913b2bcaeeb03fc299627007efd45b56da
Liam Murray <liammurray@users.noreply.github.com>
2020-02-14 21:26:53 -0800

    Add lint

--- {{cookiecutter.repo_name}}/src/util.ts
+++ {{cookiecutter.repo_name}}/src/util.ts
@@ -1,9 +1,9 @@
-export function flatten<T>(arr: T[][]) {
+export function flatten<T>(arr: T[][]): T[] {
   return arr.reduce((prev: T[], cur: T[]) => prev.concat(cur))
 }
 
 // tslint:disable-next-line function-name
-export function swap<T>(arr: T[], first: number, second: number) {
+export function swap<T>(arr: T[], first: number, second: number): T[] {
   const temp = arr[first]
   arr[first] = arr[second] // eslint-disable-line no-param-reassign
   arr[second] = temp // eslint-disable-line no-param-reassign
